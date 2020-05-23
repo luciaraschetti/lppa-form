bc4ef8465ebc79a6d7b5a24caf075746de6bac39
lucia raschetti <lucia.sn011@gmail.com>
2020-05-23 20:13:39 -0300

    LPPA-EXAM1: added displayTab function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 'use strict'
 
-var step = null; 
+var tabs = null;
+var steps = null; 
 
-var stepPosition = function(tab) { //the step that corresponds to the current tab turns violet
-    step[tab].style.backgroundColor = 'violet';
+
+var stepPosition = function(tab) { //the step that corresponds to the current tab turns violet, gets the value from displayTab()
+    steps[tab].style.backgroundColor = 'violet';
+}
+
+var displayTab = function(currentTab) { //displays the selected tab
+    tabs[currentTab].style.display = 'flex'; //changes display from none to flex to show the selected tab
+    stepPosition(currentTab);
 }
 
 window.onload = function() {
-    step = document.getElementsByClassName('step');
-    stepPosition(0);
+    tabs = document.getElementsByClassName('tab');
+    steps = document.getElementsByClassName('step');
+    displayTab(0);
 }
