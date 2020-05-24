4df388504188b90c91be803d44562c68847ffcaa
lucia raschetti <lucia.sn011@gmail.com>
2020-05-23 23:51:58 -0300

    LPPA-EXAM1: added changeTab function

--- js/app.js
+++ js/app.js
@@ -4,16 +4,20 @@ var tabs = null;
 var steps = null; 
 var backBtn = null;
 var submitBtn = null;
+var currentTab = null;
 
 var stepPosition = function(tab) { //the step that corresponds to the current tab turns violet, gets the value from displayTab()
     steps[tab].style.backgroundColor = 'violet';
 }
 
 var displayTab = function(currentTab) { //displays the selected tab
-    tabs[currentTab].style.display = 'flex'; //changes display from none to flex to show the selected tab
-    if(currentTab === 0 || currentTab === 5) { //disables 'back' button in the 1st & final tab
+    tabs[currentTab].setAttribute('style', 'display: flex !important'); //setAttribute overrides the css & changes display from none to flex to show the selected tab
+    if(currentTab === 0) { //disables 'back' button in the 1st tab
         backBtn.disabled = true;
         backBtn.style.backgroundColor = '#7c375e';
+    }else {
+        backBtn.disabled = false;
+        backBtn.style.backgroundColor = '#b14783';
     }
 
     if(currentTab === (tabs.length - 2)) { //changes 'next' to 'submit' before reaching the final message tab
@@ -25,10 +29,18 @@ var displayTab = function(currentTab) { //displays the selected tab
     stepPosition(currentTab);
 }
 
+var changeTab = function(buttonInput) { //displays the chosen tab
+    tabs[currentTab].setAttribute('style', 'display: none !important'); //setAttribute overrides the css & hides the current tab
+    currentTab += buttonInput; //increase/decrease current tab depending on clicked button value
+    console.log(buttonInput);
+    displayTab(currentTab);
+}
+
 window.onload = function() {
     tabs = document.getElementsByClassName('tab');
     steps = document.getElementsByClassName('step');
     backBtn = document.getElementById('back');
     submitBtn = document.getElementById('next');
-    displayTab(4);
+    currentTab = 0;
+    displayTab(currentTab);
 }
